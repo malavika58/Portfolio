8f6c94b685f7825b9c0db27a4cdce43f0753ce8a
malavika58 <malavikak660@gmail.com>
2025-01-21 12:59:37 +0530

    neww

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import logo from "../assets/logo.png";
+import logo from "../assets/logoM.png";
 
 const Navbar = () => {
   return (
     <nav className="mt-1 mb-20 flex items-center justify-between py-6 lg:mt-5 lg:px-20 ">
     <div className="flex flex-shrink-0 items-center" >
-        <img className="mx-2 w-20" src={logo} alt="logo"/>
+        <img className="mx-[-5] w-20" src={logo} alt="logo"/>
     </div>
     <div className="mx-4 lg:mx-8 flex items-center justify-center gap-4 text-2xl  text-white" >
         <a href="https://www.linkedin.com/in/malavika-m-540b8a29a/ "
